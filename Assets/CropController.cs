e8668afc7816d0a360a676ff2ee21a1c430a378a
BrandonO <brandonobier1998@gmail.com>
2020-08-08 23:05:33 -0700

    test

---
+++ Assets/CropController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropController : MonoBehaviour
+{
+    public string cropName = "";
+    public int timeToGrow;
+    public bool selectedCrop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cropName = gameObject.transform.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cropName);
+    }
+}
